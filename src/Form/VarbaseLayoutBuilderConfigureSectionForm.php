e219ed0a7c48a4937a5fbf6550d08e87b1abc2bd
Rajab Natshah <rajabn@gmail.com>
2020-02-12 14:57:45 +0200

    Issue #3113066: Integrate with advanced Layout Builder UX behaviors

--- src/Form/VarbaseLayoutBuilderConfigureSectionForm.php
+++ src/Form/VarbaseLayoutBuilderConfigureSectionForm.php
@@ -5,12 +5,9 @@ namespace Drupal\varbase_layout_builder\Form;
 use Drupal\layout_builder\Form\ConfigureSectionForm as OriginalConfigureSectionForm;
 
 /**
- * Class ConfigureSectionForm.
- *
- * Extend the original form to expose the layout object.
- * See https://www.drupal.org/i/3044117
+ * Class VarbaseLayoutBuilderConfigureSectionForm.
  */
-class ConfigureSectionForm extends OriginalConfigureSectionForm {
+class VarbaseLayoutBuilderConfigureSectionForm extends OriginalConfigureSectionForm {
 
   /**
    * Get the layout plugin being modified.
@@ -20,6 +17,16 @@ class ConfigureSectionForm extends OriginalConfigureSectionForm {
    */
   public function getLayout() {
     return $this->layout;
+  }
+
+  /**
+   * Get the delta value for the current section.
+   *
+   * @return int
+   *   The delta id for the current section setting form.
+   */
+  public function getDelta() {
+    return $this->delta;
   }
 
 }
