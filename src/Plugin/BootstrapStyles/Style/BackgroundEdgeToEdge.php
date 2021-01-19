e69734b721760d2964866ec822ae5e653c25b819
Rajab Natshah <rajabn@gmail.com>
2021-01-19 17:07:59 +0200

    Issue #3190716: Add Edge to Edge Background styling checkbox

--- src/Plugin/BootstrapStyles/Style/BackgroundEdgeToEdge.php
+++ src/Plugin/BootstrapStyles/Style/BackgroundEdgeToEdge.php
@@ -4,11 +4,12 @@ namespace Drupal\varbase_layout_builder\Plugin\BootstrapStyles\Style;
 
 use Drupal\bootstrap_styles\Style\StylePluginBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\layout_builder\Form\ConfigureSectionForm;
 
 /**
  * Class BackgroundEdgeToEdge.
  *
- * @package Drupal\bootstrap_styles\Plugin\Style
+ * @package Drupal\varbase_layout_builder\Plugin\Style
  *
  * @Style(
  *   id = "background_edgetoedge",
@@ -24,15 +25,31 @@ class BackgroundEdgeToEdge extends StylePluginBase {
    */
   public function buildStyleFormElements(array &$form, FormStateInterface $form_state, $storage) {
 
-    $form['background_edgetoedge'] = [
-      '#type' => 'checkbox',
-      '#title' => $this->t('Edge to Edge Background'),
-      '#default_value' => ((isset($storage['background_edgetoedge'])) ? $storage['background_edgetoedge'] : NULL),
-      '#validated' => TRUE,
-      '#attributes' => [
-        'class' => ['field-background-edge-to-edge'],
-      ],
-    ];
+    $form_object = $form_state->getFormObject();
+    if ($form_object instanceof ConfigureSectionForm) {
+      $form['background_edgetoedge'] = [
+        '#type' => 'checkbox',
+        '#title' => $this->t('Edge to Edge Background'),
+        '#default_value' => ((isset($storage['background_edgetoedge'])) ? $storage['background_edgetoedge'] : NULL),
+        '#validated' => TRUE,
+        '#attributes' => [
+          'class' => ['field-background-edge-to-edge'],
+        ],
+      ];
+    }
+    else {
+      $form['background_edgetoedge'] = [
+        '#type' => 'checkbox',
+        '#title' => $this->t('Edge to Edge Background'),
+        '#default_value' => NULL,
+        '#validated' => TRUE,
+        '#attributes' => [
+          'class' => ['field-background-edge-to-edge'],
+        ],
+        '#access' => FALSE,
+        '#default_value' => TRUE,
+      ];
+    }
 
     return $form;
   }
