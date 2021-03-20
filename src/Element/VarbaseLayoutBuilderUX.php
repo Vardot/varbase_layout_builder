2d747e6c1f4e7a13ad11b7b8d45a38c0bdf37129
Rajab Natshah <rajabn@gmail.com>
2021-03-20 11:35:50 +0200

    Issue #3202087: Add the ability to add Section title for each section and have a Section header region and Section Heading block type

--- src/Element/VarbaseLayoutBuilderUX.php
+++ src/Element/VarbaseLayoutBuilderUX.php
@@ -18,6 +18,18 @@ class VarbaseLayoutBuilderUX extends LayoutBuilder {
   protected function buildAdministrativeSection(SectionStorageInterface $section_storage, $delta) {
     $build = parent::buildAdministrativeSection($section_storage, $delta);
 
+    $storage_type = $section_storage->getStorageType();
+    $storage_id = $section_storage->getStorageId();
+    $section = $section_storage->getSection($delta);
+
+    $layout = $section->getLayout();
+    $layout_settings = $section->getLayoutSettings();
+    $section_label = !empty($layout_settings['label']) ? $layout_settings['label'] : $this->t('Section @section', ['@section' => $delta + 1]);
+
+    $layout_definition = $layout->getPluginDefinition();
+
+    $region_labels = $layout_definition->getRegionLabels();
+
     $section_label = $build['#attributes']['aria-label'];
 
     $build['configure']['#title'] = $this->t('<span class="visually-hidden">Configure @section</span>', ['@section' => $section_label]);
@@ -164,6 +176,13 @@ class VarbaseLayoutBuilderUX extends LayoutBuilder {
         }
       }
     }
+
+    foreach ($layout_definition->getRegions() as $region => $info) {
+      if ($region == 'section_header') {
+        $build['layout-builder__section'][$region]['layout_builder_add_block']['link']['#title'] = $this->t('Add heading <span class="visually-hidden">in @section, @region region</span>', ['@section' => $section_label, '@region' => $region_labels[$region]]);
+      }
+    }
+
     return $build;
   }
 
