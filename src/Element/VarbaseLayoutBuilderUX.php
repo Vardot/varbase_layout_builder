6a1f070b965795b20f93f7a4b6c03a03f9f9ada9
Ahmad  Abbad <1079-ahmad-abbad@users.noreply.drupalcode.org>
2023-01-04 11:24:58 +0300

    Issue #3317720 by Ahmad Abbad, Qusai Taha: Fix not working change of blocks ordering when the section has a heading block

--- src/Element/VarbaseLayoutBuilderUX.php
+++ src/Element/VarbaseLayoutBuilderUX.php
@@ -32,7 +32,29 @@ class VarbaseLayoutBuilderUX extends LayoutBuilder {
 
     foreach ($layout_definition->getRegions() as $region => $info) {
       if ($region == 'section_header') {
-        $build['layout-builder__section'][$region]['layout_builder_add_block']['link']['#title'] = $this->t('Add heading <span class="visually-hidden">in @section, @region region</span>', ['@section' => $section_label, '@region' => $region_labels[$region]]);
+        $build['layout-builder__section'][$region]['layout_builder_add_block']['link'] = [
+          '#type' => 'link',
+          '#title' => $this->t('Add heading <span class="visually-hidden">in @section, @region region</span>', ['@section' => $section_label, '@region' => $region_labels[$region]]),
+          '#url' => Url::fromRoute('layout_builder.choose_block',
+            [
+              'section_storage_type' => $storage_type,
+              'section_storage' => $storage_id,
+              'delta' => $delta,
+              'region' => $region,
+            ],
+            [
+              'attributes' => [
+                'class' => [
+                  'use-ajax',
+                  'layout-builder__link',
+                  'layout-builder__link--add',
+                ],
+                'data-dialog-type' => 'dialog',
+                'data-dialog-renderer' => 'off_canvas',
+              ],
+            ]
+          ),
+        ];
       }
     }
 
